584a1c34fe2056ab102041802bd560d080a0f660
Guest User <student@epicodus.com>
2015-08-04 14:47:53 -0700

    convert numbers 10 to 20 to words

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -12,21 +12,36 @@ class Fixnum
               7 => 'seven',
               8 => 'eight',
               9 => 'nine'}
-    digit2 = {10 => 'ten',
-              20 => 'twenty',
-              30 => 'thirty',
-              40 => 'fourty',
-              50 => 'fifty',
-              60 => 'sixty',
-              70 => 'seventy',
-              80 => 'eighty',
-              90 => 'ninety'}
-    value = self.to_s
-    value.each_char do |char|
-#binding.pry
-      if value.length() == 1
-        word = digit1.fetch(char.to_i)
-  #      digit1.fetch(char.to_i)
+
+    digits_to_20 = {10 => 'ten',
+              11 => 'eleven',
+              12 => 'twelve',
+              13 => 'thirteen',
+              14 => 'fourteen',
+              15 => 'fifteen',
+              16 => 'sixteen',
+              17 => 'seventeen',
+              18 => 'eighteen',
+              19 => 'nineteen',
+              20 => 'twenty'}
+
+    # digit2 = {10 => 'ten',
+    #           20 => 'twenty',
+    #           30 => 'thirty',
+    #           40 => 'fourty',
+    #           50 => 'fifty',
+    #           60 => 'sixty',
+    #           70 => 'seventy',
+    #           80 => 'eighty',
+    #           90 => 'ninety'}
+    if self >= 10 && self <= 20
+      word = digits_to_20.fetch(self)
+    else
+      value = self.to_s
+      value.each_char do |char|
+        if value.length() == 1
+          word = digit1.fetch(char.to_i)
+        end
       end
     end
     word
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -3,7 +3,11 @@ require('pry')
 require('numbers_to_words')
 
 describe('Fixnum#numbers_to_words') do
-  it('takes a number and converts to word') do
+  it('takes a single digit and converts to word') do
     expect((1).numbers_to_words()).to(eq('one'))
+  end
+
+  it('takes a value between 10 and 20 and converts to word') do
+    expect((15).numbers_to_words()).to(eq('fifteen'))
   end
 end
